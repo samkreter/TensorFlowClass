3f0f6109fec06ecc36a62600210564b450506118
Sam Kreter <samkreter@gmail.com>
2016-05-17 18:55:54 -0500

    added reformer for the dataset

--- hw2.py
+++ hw2.py
@@ -30,9 +30,12 @@ def reformat(dataset, labels):
   # Map 0 to [1.0, 0.0, 0.0 ...], 1 to [0.0, 1.0, 0.0 ...]
   labels = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
   return dataset, labels
+
 train_dataset, train_labels = reformat(train_dataset, train_labels)
 valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
 test_dataset, test_labels = reformat(test_dataset, test_labels)
 print('Training set', train_dataset.shape, train_labels.shape)
 print('Validation set', valid_dataset.shape, valid_labels.shape)
 print('Test set', test_dataset.shape, test_labels.shape)
+
+
